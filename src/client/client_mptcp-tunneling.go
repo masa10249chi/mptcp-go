e52d89ca1645e949a0029ce3f2be270d063869c6
masa10249chi <masato.nishiguchi.ty@hco.ntt.co.jp>
2021-05-10 02:48:04 +0900

    Hotfix; Reassembling messages transported on MPTCP

--- src/client/client_mptcp-tunneling.go
+++ src/client/client_mptcp-tunneling.go
@@ -1,52 +1,65 @@
 package main
 
 import (
-        "flag"
-        "fmt"
-        "net"
-        "os"
-        "sync"
-        "syscall"
-        "time"
+    "flag"
+    "fmt"
+    "net"
+    "os"
+    "sync"
+    "syscall"
+    "time"
 
-        "github.com/google/gopacket"
-        "github.com/google/gopacket/pcap"
+    "github.com/google/gopacket"
+    "github.com/google/gopacket/pcap"
 )
 
 const (
-        MPTCP_SCHEDULER         = 43
-        MPTCP_PATH_MANAGER      = 44
+    MPTCP_SCHEDULER     = 43
+    MPTCP_PATH_MANAGER  = 44
+
+    DELIMITER_FIN       = "<mptcp-go-fin>"
 )
 
 func handleMPTCPDecap(conn net.Conn, handle *pcap.Handle, wg *sync.WaitGroup) {
     defer wg.Done()
 
-        conn.SetReadDeadline( time.Time{} /*time.Now().Add(10 * time.Second)*/ )
-        messageBuf := make([]byte, 0xFFFF)
-        for {
-        //messageBuf := make([]byte, 1518)
-        messageLen, err := conn.Read(messageBuf)
+    conn.SetReadDeadline( time.Time{} /*time.Now().Add(10 * time.Second)*/ )
+    
+    message_buf := make([]byte, 0xFFFF)
+    write_buf := []byte{}
+    for {
+        message_len, err := conn.Read(message_buf)
         if err != nil {
             fmt.Println(err)
             fmt.Fprintf(os.Stderr, "Wrong message.\n")
             os.Exit(1)
         }
-        err = handle.WritePacketData(messageBuf[:messageLen])
-        if err != nil {
-            fmt.Fprintf(os.Stderr, "Failed to decap message.\n")
-            os.Exit(1)
+        write_buf = append(write_buf, message_buf[:message_len]...)
+
+        if strings.Contains(string(message_buf[:message_len]), DELIMITER_FIN) {
+            write_buf = write_buf[0:bytes.Index(write_buf, []byte(DELIMITER_FIN))]
+
+            err = handle.WritePacketData(write_buf)
+            if err != nil {
+                fmt.Fprintf(os.Stderr, "Failed to decap message.\n")
+                os.Exit(1)
+            }
+
+            write_buf = write_buf[:0]
         }
     }
 }
 
 func handleMPTCPEncap(conn net.Conn, handle *pcap.Handle, wg *sync.WaitGroup) {
-        defer wg.Done()
+    defer wg.Done()
 
-        conn.SetWriteDeadline( time.Time{} /*time.Now().Add(10 * time.Second)*/ )
+    conn.SetWriteDeadline( time.Time{} /*time.Now().Add(10 * time.Second)*/ )
 
-        packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
+    packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
     for packet := range packetSource.Packets() {
-        _, err := conn.Write(packet.Data())
+        packet_with_delimiter := append(packet.Data(), DELIMITER_FIN...)
+        
+        _, err := conn.Write(packet_with_delimiter)
         if err != nil {
             fmt.Fprintf(os.Stderr, "Failed to encap message.\n")
             os.Exit(1)
@@ -55,73 +68,73 @@ func handleMPTCPEncap(conn net.Conn, handle *pcap.Handle, wg *sync.WaitGroup) {
 }
 
 func main() {
-        var  (
-                client_ip       = flag.String("client_ip", "", "Client IP address")
-                pathmanager = flag.String("pathmanager", "ndiffports", "MPTCP pathmanager option")
-                scheduler       = flag.String("scheduler", "default", "MPTCP scheduler option")
-                server_ip   = flag.String("server_ip", "", "Server IP address")
-                server_port = flag.String("server_port", "", "Server port number")
+    var (
+        client_ip   = flag.String("client_ip", "", "Client IP address")
+        pathmanager = flag.String("pathmanager", "ndiffports", "MPTCP pathmanager option")
+        scheduler   = flag.String("scheduler", "default", "MPTCP scheduler option")
+        server_ip   = flag.String("server_ip", "", "Server IP address")
+        server_port = flag.String("server_port", "", "Server port number")
 
-                pcap_device     string = "eth0"
-                snapshot_len    int32  = 0xFFFF
-                promiscuous     bool   = true
-                // timeout              time.Duration = -1 * time.Second
-                handle                  *pcap.Handle
+        pcap_device     string = "eth0"
+        snapshot_len    int32  = 0xFFFF
+        promiscuous     bool   = true
+        // timeout      time.Duration = -1 * time.Second
+        handle          *pcap.Handle
 
-                err     error
-        )
+        err error
+    )
 
-        flag.Parse()
+    flag.Parse()
 
-        my_addr, err := net.ResolveIPAddr("ip4", *client_ip)
-        if *client_ip == "" || err != nil {
-                fmt.Fprintf(os.Stderr, "Invalid client address.\n")
-                os.Exit(1)
-        }
+    my_addr, err := net.ResolveIPAddr("ip4", *client_ip)
+    if *client_ip == "" || err != nil {
+        fmt.Fprintf(os.Stderr, "Invalid client address.\n")
+        os.Exit(1)
+    }
 
-        srv_addr, err := net.ResolveTCPAddr("tcp", *server_ip+ ":" + *server_port)
-        if *server_ip == "" || *server_port == "" || err != nil {
+    srv_addr, err := net.ResolveTCPAddr("tcp", *server_ip+ ":" + *server_port)
+    if *server_ip == "" || *server_port == "" || err != nil {
         fmt.Fprintf(os.Stderr, "Invalid server address.\n")
         os.Exit(1)
-        }
-        srv_addrStr := *server_ip+ ":" + *server_port
+    }
+    srv_addrStr := *server_ip+ ":" + *server_port
 
-        fmt.Printf("Client start ! (Client: %v <-> Server: %v:%v)\n", my_addr.IP, srv_addr.IP, srv_addr.Port)
+    fmt.Printf("Client start ! (Client: %v <-> Server: %v:%v)\n", my_addr.IP, srv_addr.IP, srv_addr.Port)
 
-        my_dialer := new(net.Dialer)
-        my_dialer.Control = func(network, address string, c syscall.RawConn) error {
+    my_dialer := new(net.Dialer)
+    my_dialer.Control = func(network, address string, c syscall.RawConn) error {
         var err error
         c.Control(func(fd uintptr) {
-                err = syscall.SetsockoptString(int(fd), syscall.SOL_TCP, MPTCP_SCHEDULER, *scheduler)
-                        if err == nil {
-                                err = syscall.SetsockoptString(int(fd), syscall.SOL_TCP, MPTCP_PATH_MANAGER, *pathmanager)
-                }
-                })
+            err = syscall.SetsockoptString(int(fd), syscall.SOL_TCP, MPTCP_SCHEDULER, *scheduler)
+            if err == nil {
+                err = syscall.SetsockoptString(int(fd), syscall.SOL_TCP, MPTCP_PATH_MANAGER, *pathmanager)
+            }
+        })
         return err
     }
 
-        conn, err := my_dialer.Dial("tcp", srv_addrStr)
+    conn, err := my_dialer.Dial("tcp", srv_addrStr)
     if err != nil {
-                fmt.Fprintf(os.Stderr, "Failed to dial TCP.\n")
-                os.Exit(1)
-        }
-        defer conn.Close()
+        fmt.Fprintf(os.Stderr, "Failed to dial TCP.\n")
+        os.Exit(1)
+    }
+    defer conn.Close()
 
-        fmt.Printf("Hello, MPTCP Connection !\n")
+    fmt.Printf("Hello, MPTCP Connection !\n")
 
-        handle, err = pcap.OpenLive(pcap_device, snapshot_len, promiscuous, pcap.BlockForever /*timeout*/ )
-        if err != nil {
-                fmt.Fprintf(os.Stderr, "Failed to open pcap device.\n")
-                os.Exit(1)
-        }
-        defer handle.Close()
-        _ = handle.SetBPFFilter("src host 192.168.1.1")
+    handle, err = pcap.OpenLive(pcap_device, snapshot_len, promiscuous, pcap.BlockForever /*timeout*/ )
+    if err != nil {
+        fmt.Fprintf(os.Stderr, "Failed to open pcap device.\n")
+        os.Exit(1)
+    }
+    defer handle.Close()
+    //_ = handle.SetBPFFilter("src host 192.168.1.1")
 
-        wg := &sync.WaitGroup{}
-        wg.Add(2)
+    wg := &sync.WaitGroup{}
+    wg.Add(2)
 
-        go handleMPTCPDecap(conn, handle, wg)
-        go handleMPTCPEncap(conn, handle, wg)
+    go handleMPTCPDecap(conn, handle, wg)
+    go handleMPTCPEncap(conn, handle, wg)
 
-        wg.Wait()
+    wg.Wait()
 }
--- src/server/server_mptcp-tunneling.go
+++ src/server/server_mptcp-tunneling.go
@@ -1,111 +1,126 @@
 package main
 
 import (
-        "flag"
-        "fmt"
-        "net"
-        "os"
-        "sync"
-        "time"
+    "flag"
+    "fmt"
+    "net"
+    "os"
+    "sync"
+    "time"
 
-        "github.com/google/gopacket"
-        "github.com/google/gopacket/pcap"
+    "github.com/google/gopacket"
+    "github.com/google/gopacket/pcap"
+)
+
+const (
+    DELIMITER_FIN = "<mptcp-go-fin>"
 )
 
 func handleMPTCPDecap(conn net.Conn, handle *pcap.Handle, wg *sync.WaitGroup) {
-        defer wg.Done()
+    defer wg.Done()
 
-        conn.SetReadDeadline( time.Time{} /*time.Now().Add(10 * time.Second)*/ )
-        messageBuf := make([]byte, 0xFFFF)
-        for {
-                // messageBuf := make([]byte, 0xFFFF)
-                messageLen, err := conn.Read(messageBuf)
-                if err != nil {
-                fmt.Println(err)
-                        fmt.Fprintf(os.Stderr, "Wrong message.\n")
-                os.Exit(1)
-                }
-                err = handle.WritePacketData(messageBuf[:messageLen])
+    conn.SetReadDeadline( time.Time{} /*time.Now().Add(10 * time.Second)*/ )
+    
+    message_buf := make([]byte, 0xFFFF)
+    write_buf := []byte{}
+    for {
+        message_len, err := conn.Read(message_buf)
         if err != nil {
-                        fmt.Fprintf(os.Stderr, "Failed to decap message.\n")
-                        os.Exit(1)
+            fmt.Println(err)
+            fmt.Fprintf(os.Stderr, "Wrong message.\n")
+            os.Exit(1)
         }
+        write_buf = append(write_buf, message_buf[:message_len]...)
+
+        if strings.Contains(string(message_buf[:message_len]), DELIMITER_FIN) {
+            write_buf = write_buf[0:bytes.Index(write_buf, []byte(DELIMITER_FIN))]
+
+            err = handle.WritePacketData(write_buf)
+            if err != nil {
+                fmt.Fprintf(os.Stderr, "Failed to decap message.\n")
+                os.Exit(1)
+            }
+
+            write_buf = write_buf[:0]
         }
+    }
 }
 
 func handleMPTCPEncap(conn net.Conn, handle *pcap.Handle, wg *sync.WaitGroup) {
-        defer wg.Done()
+    defer wg.Done()
 
-        conn.SetWriteDeadline( time.Time{} /*time.Now().Add(10 * time.Second)*/ )
+    conn.SetWriteDeadline( time.Time{} /*time.Now().Add(10 * time.Second)*/ )
 
-        packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
-        for packet := range packetSource.Packets() {
-                _, err := conn.Write(packet.Data())
+    packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
+    for packet := range packetSource.Packets() {
+        packet_with_delimiter := append(packet.Data(), DELIMITER_FIN...)
+        
+        _, err := conn.Write(packet_with_delimiter)
         if err != nil {
-                fmt.Fprintf(os.Stderr, "Failed to encap message.\n")
-                os.Exit(1)
+            fmt.Fprintf(os.Stderr, "Failed to encap message.\n")
+            os.Exit(1)
         }
-        }
-}
-
-func main() {
-    var  (
-                server_ip   = flag.String("server_ip", "", "Server IP address")
-                server_port = flag.String("server_port", "", "Server port number")
-
-                pcap_device     string = "eth0"
-                snapshot_len    int32  = 0xFFFF
-                promiscuous     bool   = true
-                // timeout         time.Duration = -1 * time.Second
-                handle          *pcap.Handle
-
-                err error
-    )
-
-        flag.Parse()
-
-        srv_addr, err := net.ResolveTCPAddr("tcp", *server_ip+ ":" + *server_port)
-        if *server_ip == "" || *server_port == "" || err != nil {
-        fmt.Fprintf(os.Stderr, "Invalid server address.\n")
-        os.Exit(1)
-        }
-
-        fmt.Printf("Server start ! (Server: %v:%v)\n", srv_addr.IP, srv_addr.Port)
-
-        listner, err := net.ListenTCP("tcp", srv_addr)
-        if err != nil {
-        fmt.Fprintf(os.Stderr, "Failed to listen TCP.\n")
-        os.Exit(1)
-        }
-
-        handle, err = pcap.OpenLive(pcap_device, snapshot_len, promiscuous, pcap.BlockForever /*timeout*/ )
-        if err != nil {
-                fmt.Fprintf(os.Stderr, "Failed to open pcap device.\n")
-                os.Exit(1)
-        }
-        defer handle.Close()
-        _ = handle.SetBPFFilter("src host 192.168.1.2")
-
-        for {
-                conn, err := listner.Accept()
-                if err != nil {
-                        continue
-                }
-
-                go handleConnection(conn, handle)
-        }
+    }
 }
 
 func handleConnection(conn net.Conn, handle *pcap.Handle) {
-        defer conn.Close()
+    defer conn.Close()
 
-        fmt.Println("Client accept!")
+    fmt.Println("Client accept!")
 
-        wg := &sync.WaitGroup{}
-        wg.Add(2)
+    wg := &sync.WaitGroup{}
+    wg.Add(2)
 
-        go handleMPTCPDecap(conn, handle, wg)
-        go handleMPTCPEncap(conn, handle, wg)
+    go handleMPTCPDecap(conn, handle, wg)
+    go handleMPTCPEncap(conn, handle, wg)
 
-        wg.Wait()
+    wg.Wait()
+}
+
+func main() {
+    var (
+        server_ip   = flag.String("server_ip", "", "Server IP address")
+        server_port = flag.String("server_port", "", "Server port number")
+
+        pcap_device     string = "eth0"
+        snapshot_len    int32  = 0xFFFF
+        promiscuous     bool   = true
+        // timeout      time.Duration = -1 * time.Second
+        handle          *pcap.Handle
+
+        err error
+    )
+
+    flag.Parse()
+
+    srv_addr, err := net.ResolveTCPAddr("tcp", *server_ip+ ":" + *server_port)
+    if *server_ip == "" || *server_port == "" || err != nil {
+        fmt.Fprintf(os.Stderr, "Invalid server address.\n")
+        os.Exit(1)
+    }
+
+    fmt.Printf("Server start ! (Server: %v:%v)\n", srv_addr.IP, srv_addr.Port)
+
+    listner, err := net.ListenTCP("tcp", srv_addr)
+    if err != nil {
+        fmt.Fprintf(os.Stderr, "Failed to listen TCP.\n")
+        os.Exit(1)
+    }
+
+    handle, err = pcap.OpenLive(pcap_device, snapshot_len, promiscuous, pcap.BlockForever /*timeout*/ )
+    if err != nil {
+        fmt.Fprintf(os.Stderr, "Failed to open pcap device.\n")
+        os.Exit(1)
+    }
+    defer handle.Close()
+    // _ = handle.SetBPFFilter("src host 192.168.1.2")
+
+    for {
+        conn, err := listner.Accept()
+        if err != nil {
+            continue
+        }
+
+        go handleConnection(conn, handle)
+    }
 }
